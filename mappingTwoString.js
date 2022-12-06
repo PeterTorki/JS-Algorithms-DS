e7aaa1f7ab28288aaa8ae6c72d2cb514b1832dff
Peter <peter.j.torki@gmail.com>
2022-12-06 12:21:23 +0200

    stringTwoMapping

---
+++ mappingTwoString.js
@@ -0,0 +1,15 @@
+isGood = function (s, t) {
+    let freq = new Map()
+    for(let i = 0 ; i < s.length; i++)  freq.set(s[i],  t[i])
+    
+    for(let i = 0 ; i < s.length; i++)  {
+        if(freq[s[i]] === undefined) 
+            freq.set(s[i], t[i])
+    }
+    
+    for(let i = 0 ; i < s.length ; i++) {
+            if(freq.get(s[i]) === t[i]) continue
+            else return false
+    }
+    return true
+ }
